9ae33acc502914748bf04c38ae4ba906c303a269
hemanthrishee <88771647+hemanthrishee@users.noreply.github.com>
2023-09-22 02:04:21 +0530

    initialised routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Login from "./pages/Login";
+import Project from "./pages/Project";
+import AddProject from "./pages/AddProject";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/addproject" element={<AddProject />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </Router>
   );
 }
 
