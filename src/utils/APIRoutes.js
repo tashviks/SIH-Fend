aee903813854c6c32fde20392e5e2eeb999b3d04
hemanthrishee <88771647+hemanthrishee@users.noreply.github.com>
2023-09-28 03:07:36 +0530

    apiroutes

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -2,3 +2,5 @@ export const host = "http://localhost:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
+export const addProjectRoute = `${host}/api/auth/addproject`;
+export const getUnivProjects = `${host}/api/auth/univprojects`;
